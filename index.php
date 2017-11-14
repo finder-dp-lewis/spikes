ebd300f97bb817c67deee7a5d07cba172297175c
Tomasz Kopacki <tomasz@kopacki.net>
2017-11-14 14:17:52 +1100

    add root path support

--- index.php
+++ index.php
@@ -15,19 +15,18 @@
  */
 
 function create_posttype() {
- register_post_type( 'review',
-   array(
-     'labels' => array(
-       'name' => __( 'Reviews' ),
-       'singular_name' => __( 'Review' ),
-       'add_new_item' => __('Add new review'),
-     ),
-     'public' => true,
-     'has_archive' => true,
-     'rewrite' => array('slug' => 'reviews'),
-     'supports' => array( 'title', 'editor', 'thumbnail', 'revisions', )
-   )
- );
+    register_post_type( 'review',
+        array(
+            'labels' => array(
+                'name' => __( 'Reviews' ),
+                'singular_name' => __( 'Review' ),
+                'add_new_item' => __('Add new review'),
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array( 'title', 'editor', 'thumbnail', 'revisions' )
+        )
+    );
 }
 
 // Set up the editor
@@ -134,6 +133,47 @@ function templatise_revision_field( $value, $field ) {
 	return get_metadata( 'post', $revision->ID, $field, true );
 }
 
+
+//  https://kellenmace.com/remove-custom-post-type-slug-from-permalinks/
+/**
+ * Have WordPress match postname to any of our public post types (post, page, review).
+ * All of our public post types can have /post-name/ as the slug, so they need to be unique across all posts.
+ * By default, WordPress only accounts for posts and pages where the slug is /post-name/.
+ *
+ * @param $query The current query.
+ */
+function gp_add_cpt_post_names_to_main_query( $query ) {
+	// Bail if this is not the main query.
+	if ( ! $query->is_main_query() ) {
+		return;
+	}
+	// Bail if this query doesn't match our very specific rewrite rule.
+	if ( ! isset( $query->query['page'] ) || 2 !== count( $query->query ) ) {
+		return;
+	}
+	// Bail if we're not querying based on the post name.
+	if ( empty( $query->query['name'] ) ) {
+		return;
+	}
+	// Add CPT to the list of post types WP will include when it queries based on the post name.
+	$query->set( 'post_type', array( 'post', 'page', 'review' ) );
+}
+add_action( 'pre_get_posts', 'gp_add_cpt_post_names_to_main_query' );
+
+/**
+ * Remove the slug from published post permalinks. Only affect our custom post type, though.
+ */
+function gp_remove_cpt_slug( $post_link, $post, $leavename ) {
+
+    if ( 'review' != $post->post_type || 'publish' != $post->post_status ) {
+        return $post_link;
+    }
+
+    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
+
+    return $post_link;
+}
+add_filter( 'post_type_link', 'gp_remove_cpt_slug', 10, 3 );
 
 // Hook into Wordpress
 add_action('admin_init', 'templatise_add_metabox', 1);
